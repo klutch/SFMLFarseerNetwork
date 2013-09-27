6ed16cbbc5a37b9a861ed269f2ee23f19aba2bbe
Graeme Collins <graeme.collins.0@gmail.com>
2013-09-27 10:18:47 -0500

    Initial commit.

---
+++ SFML_Farseer_Network/Game.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using SFML.Graphics;
+using SFML.Window;
+
+namespace SFML_Farseer_Network
+{
+    public class Game : IDisposable
+    {
+        private RenderWindow _window;
+
+        public Game()
+        {
+            _window = new RenderWindow(new VideoMode(800, 600), "Farseer Network Test");
+            _window.Closed += new EventHandler(_window_Closed);
+        }
+
+        void _window_Closed(object sender, EventArgs e)
+        {
+            _window.Close();
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public void run()
+        {
+            while (_window.IsOpen())
+            {
+                update();
+                draw();
+            }
+        }
+
+        public void update()
+        {
+            _window.DispatchEvents();
+        }
+
+        public void draw()
+        {
+            _window.Clear(Color.Black);
+            _window.Display();
+        }
+    }
+}
---
+++ SFML_Farseer_Network/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SFML_Farseer_Network
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (Game game = new Game())
+            {
+                game.run();
+            }
+        }
+    }
+}
